1f4adcbf28a3371778540b4b26623a43e14413c3
ahmedelhalwagy3 <ahmedelhalwagy31@gmail.com>
2024-08-13 05:14:47 +0300

    Add themes folder with ThemeContext.tsx

---
+++ src/themes/ThemeContext.tsx
@@ -0,0 +1,29 @@
+// ThemeContext.tsx
+import React, { createContext, useState, ReactNode, useContext } from 'react';
+
+interface ThemeContextType {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = () => setDarkMode((prev) => !prev);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('error');
+  }
+  return context;
+};
